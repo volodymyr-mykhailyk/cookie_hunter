b296424078ec1beccb9ec00649c45f64b62015eb
Anton Mishchuk <amishchuk@nravo.com>
2013-10-09 17:58:22 +0300

    devise for hunter

---
+++ app/models/hunter.rb
@@ -0,0 +1,4 @@
+class Hunter < ActiveRecord::Base
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+end
---
+++ spec/models/hunter_spec.rb
@@ -0,0 +1,5 @@
+require 'spec_helper'
+
+describe Hunter do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
