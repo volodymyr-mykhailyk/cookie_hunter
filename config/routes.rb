87cce58e0dda3239e0c2725a02e5d07088b1d1a4
Anton Mishchuk <amishchuk@nravo.com>
2013-10-24 22:56:54 +0300

    root to hunting

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ CookieHunter::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root :to => "home#index"
+  root :to => "huntings#show"
 
   resource :hunting, only: :show
   resource :stockpile, only: [] do
