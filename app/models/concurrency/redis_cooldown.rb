d91a7d19c1f0b1b546b0ef32d094d9d77ebcfa7a
vmykhailyk <vmykhailyk@meni.mobi>
2013-10-26 09:00:59 +0300

    fix redis problem on heroku

--- app/models/concurrency/redis_cooldown.rb
+++ app/models/concurrency/redis_cooldown.rb
@@ -10,6 +10,14 @@ module Concurrency::RedisCooldown
   protected
   def obtain_cooldown(name, cooldown_time)
     lock_end_time = (Time.now.to_f + cooldown_time + 1)
-    redis.set(name, lock_end_time, {px: cooldown_time, nx: true})
+    if redis.method(:set).arity == 3
+      redis.set(name, lock_end_time, {px: cooldown_time, nx: true})
+    else
+      #  workaround for heroku old redis
+      #locks are not working properly in this mode
+      return false unless redis.setnx(name, lock_end_time)
+      redis.pexpire(name, cooldown_time)
+      true
+    end
   end
 end
--- app/models/concurrency/redis_locking.rb
+++ app/models/concurrency/redis_locking.rb
@@ -16,6 +16,14 @@ module Concurrency::RedisLocking
   # return true if lock obtained or false if already exists
   def obtain_lock(name, lock_time)
     lock_end_time = (Time.now.to_f + lock_time + 1)
-    redis.set(name, lock_end_time, {ex: lock_time, nx: true})
+    if redis.method(:set).arity == 3
+      redis.set(name, lock_end_time, {ex: lock_time, nx: true})
+    else
+      #workaround for heroku old redis
+      #locks are not working properly in this mode
+      return false unless redis.setnx(name, lock_end_time)
+      redis.pexpire(name, cooldown_time)
+      true
+    end
   end
 end
